6ffdfa612db14f4af86a33ebfbbc0b9ca0503181
Stanislav Smolevich <smolestan@Stanislavs-MacBook-Pro.local>
2019-06-03 10:39:39 +0300

    Updated static root

--- smolearn/settings.py
+++ smolearn/settings.py
@@ -136,6 +136,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
