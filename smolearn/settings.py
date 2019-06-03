452330e30381d78c6c7292efaefadc7aaf7a9ea8
Stanislav Smolevich <smolestan@Stanislavs-MacBook-Pro.local>
2019-06-03 11:24:42 +0300

    Updated settings

--- smolearn/settings.py
+++ smolearn/settings.py
@@ -20,12 +20,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '@5#q7d$^lcn@kj7zq7m^zg3%6(uip7!t57&-8v8d*rmc!_#3#f'
+# SECRET_KEY = '@5#q7d$^lcn@kj7zq7m^zg3%6(uip7!t57&-8v8d*rmc!_#3#f'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['smolearn.herokuapp.com']
 
 
 # Application definition
@@ -143,6 +144,10 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
 
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
